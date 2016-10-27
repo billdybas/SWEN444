ddac0583dc8415097fa7dfcd8a311801937a012a
Brandon <bas4691@g.rit.edu>
2016-10-27 01:40:58 -0400

    Basic employer offer list page

--- public/javascripts/Employee.js
+++ public/javascripts/Employee.js
@@ -2,7 +2,12 @@
  * Created by Brandon on 10/26/2016.
  */
 
-var fakeData={
+var fakeUser = {
+    name: "Donald Trump",
+    profilePicture: "pictures/trump.jpg"
+}
+
+var fakeData = {
     jobName: "BEST JOBEVER",
     employerName: "Employer Name",
     description: "These are the deatils for the best job ever.  It's the best job you'll ever get.  Let me tell you, nobody knows jobs like I do.  I know all of the best jobs and the best places to get jobs.  And that's why I should be president of the United States.  And I'll stop ISIS.  If she could do that, She would've done it.  But she didn't.  And everybody should use TEMPiN.",
@@ -11,55 +16,57 @@ var fakeData={
 };
 
 var employeeView;
-var jobList = [fakeData,fakeData,fakeData,fakeData,fakeData,fakeData,fakeData,fakeData,fakeData,fakeData];
+var jobList = [fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData];
 
-function createJobPanels(){
+function createJobPanels() {
     employeeView = $("#employeeView")[0]
     jobList.forEach(createSingleJobPanel);
 }
 
-function createSingleJobPanel(jobData, index){
+function createSingleJobPanel(jobData, index) {
     //creating panel
     var panel = document.createElement("div");
-    panel.classList="jobPanel col s12";
-    $(panel).on("click touch",function(){showSingleJob(index)});
+    panel.classList = "jobPanel col s12";
+    $(panel).on("click touch", function () {
+        showSingleJob(index)
+    });
 
     //creating top line
     var topLine = document.createElement("div");
-    topLine.classList="panelRow";
+    topLine.classList = "panelRow";
     //creating and adding jobName
     var jobName = document.createElement("div");
-    jobName.classList="panelJobName";
-    jobName.innerHTML=jobData.jobName;
+    jobName.classList = "panelJobName";
+    jobName.innerHTML = jobData.jobName;
     topLine.appendChild(jobName);
     //creating and adding postTime
     var postTime = document.createElement("div");
-    postTime.classList="panelPostTime";
-    postTime.innerHTML = "Posted "+jobData.postTime;
+    postTime.classList = "panelPostTime";
+    postTime.innerHTML = "Posted " + jobData.postTime;
     topLine.appendChild(postTime);
 
 
     //creating middle line
     var middleLine = document.createElement("div");
-    middleLine.classList="panelRow";
+    middleLine.classList = "panelRow";
     //creating and adding employerName
     var employerName = document.createElement("div");
-    employerName.classList="panelEmployerName";
-    employerName.innerHTML=jobData.employerName;
+    employerName.classList = "panelEmployerName";
+    employerName.innerHTML = jobData.employerName;
     middleLine.appendChild(employerName);
     //creating and adding salary
     var salary = document.createElement("div");
-    salary.classList="panelSalary";
-    salary.innerHTML="$"+jobData.salary+"/hr";
+    salary.classList = "panelSalary";
+    salary.innerHTML = "$" + jobData.salary + "/hr";
     middleLine.appendChild(salary);
 
     //creating bottom line
-    var bottomLine=document.createElement("div");
-    bottomLine.classList="panelRow";
+    var bottomLine = document.createElement("div");
+    bottomLine.classList = "panelRow";
     //creating description
     var description = document.createElement("div");
-    description.classList="panelDescription";
-    description.innerHTML=jobData.description;
+    description.classList = "panelDescription";
+    description.innerHTML = jobData.description;
     bottomLine.appendChild(description);
 
     //putting panel together
@@ -74,19 +81,21 @@ function createSingleJobPanel(jobData, index){
     //adding divider
     var divider = document.createElement("div");
     divider.classList.add("divider");
-    divider.style.width="100%";
+    divider.style.width = "100%";
     panelView.appendChild(divider);
 }
 
-function showSingleJob(index){
-    $("#panelView")[0].style.display="none";
-    $("#singleJobView")[0].style.display="block";
+function showSingleJob(index) {
+    $("#panelView")[0].style.display = "none";
+    $("#singleJobView")[0].style.display = "block";
     showBackButton();
-    $("#backButton").one("click touch", function(){showPanels()});
+    $("#backButton").one("click touch", function () {
+        showPanels()
+    });
 }
 
-function showPanels(){
-    $("#singleJobView")[0].style.display="none";
-    $("#panelView")[0].style.display="block";
+function showPanels() {
+    $("#singleJobView")[0].style.display = "none";
+    $("#panelView")[0].style.display = "block";
     showHamburger()
 }
--- public/javascripts/Employer.js
+++ public/javascripts/Employer.js
@@ -1,3 +1,80 @@
 /**
  * Created by Brandon on 10/26/2016.
  */
+
+var fakeData = {
+    jobName: "BEST JOBEVER",
+    employerName: "Employer Name",
+    description: "These are the deatils for the best job ever.  It's the best job you'll ever get.  Let me tell you, nobody knows jobs like I do.  I know all of the best jobs and the best places to get jobs.  And that's why I should be president of the United States.  And I'll stop ISIS.  If she could do that, She would've done it.  But she didn't.  And everybody should use TEMPiN.",
+    postTime: "5 hours ago",
+    salary: "5",
+    isOpen:"Open",
+    numApps:"2"
+};
+
+var employerView;
+var jobList = [fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData, fakeData];
+
+function createOfferPanels() {
+    employerView = $("#employeeView")[0];
+    jobList.forEach(createSingleOffer);
+}
+
+function createSingleOffer(data,index){
+    var panel = document.createElement("div");
+    panel.classList = "jobPanel col s12";
+    $(panel).on("click touch", function () {
+        showSingleOffer(index);
+    });
+
+    //creating top line
+    var topLine = document.createElement("div");
+    topLine.classList = "panelRow";
+    //creating and adding jobName
+    var jobName = document.createElement("div");
+    jobName.classList = "panelJobName";
+    jobName.innerHTML = data.jobName;
+    topLine.appendChild(jobName);
+    //creating and adding postTime
+    var postTime = document.createElement("div");
+    postTime.classList = "panelPostTime";
+    postTime.innerHTML = "Posted " + data.postTime;
+    topLine.appendChild(postTime);
+
+    //creating bottom line
+    var bottomLine = document.createElement("div");
+    bottomLine.classList = "panelRow";
+    //creating description
+    var numApps = document.createElement("div");
+    numApps.classList = "panelEmployerName";
+    numApps.innerHTML = data.numApps + " Applications Submitted";
+    bottomLine.appendChild(numApps);
+    //creating vertical dropdown
+    var dropdown = document.createElement("a");
+    dropdown.style.color="#000000";
+    dropdown.href="#";
+    dropdown.classList="panelSalary";
+    var dots = document.createElement("i");
+    dots.classList="material-icons offerDots";
+    dots.innerHTML="more_vert";
+    dropdown.appendChild(dots);
+    bottomLine.appendChild(dropdown);
+
+    //putting panel together
+    panel.appendChild(topLine);
+    panel.appendChild(bottomLine);
+
+    var offerPanel = $("#offerPanel")[0];
+    //adding panel
+    offerPanel.appendChild(panel);
+
+    //adding divider
+    var divider = document.createElement("div");
+    divider.classList.add("divider");
+    divider.style.width = "100%";
+    offerPanel.appendChild(divider);
+}
+
+function showSingleOffer(index){
+    console.log(index);
+}
